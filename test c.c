65b75ca7d92a4ddfa9f87d126291161db26bbde6
Hepssylon <102019165+Hepssylon@users.noreply.github.com>
2022-06-16 10:07:15 +0200

    Create test c.c

---
+++ test c.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "solution.h"
+
+int main() {
+  
+    printf("%s\n",erase(""));
+}
+
+char* erase(char* chaine) {
+    int i = 0, j = 0;
+    int taille = strlen(chaine);
+    char* newChaine = malloc( sizeof(char) * taille);
+    
+    while(chaine[i] != '\0'){
+        if(chaine[i] != ' '){
+          newChaine[j] = chaine[i];
+          j++;
+
+        }else if(chaine[i] == ' ' && chaine[i+1] == ' '){
+          newChaine[j] = chaine[i];
+          j++;
+
+        }else if( i > 0 && chaine[i] == ' ' && chaine[i-1] == ' '){
+          newChaine[j] = chaine[i];
+          j++;
+
+        }
+    i++;
+
+    }
+    
+    newChaine[j] = '\0';
+
+    return newChaine;
+}
