276072cd3e5464c1b325e79b46a6176df3bacb2a
Hepssylon <102019165+Hepssylon@users.noreply.github.com>
2022-06-17 18:03:46 +0200

    Update test java.java

--- test java.java
+++ test java.java
@@ -3,21 +3,19 @@ package eraser;
 public class Eraser {
 
 	public static void main(String[] args) {
-		String str = erase("Cou cou  J M  B");
-		System.out.println(str);
+		erase("Cou cou  J M  B");
 	}
 
-	public static String erase(String str) {
-				StringBuilder StrB = new StringBuilder(str);
-				int taille = StrB.length();
-				for(int i = 0;i< taille;i++) {
-					if(StrB.charAt(i)== (char)32 && StrB.charAt(Math.min(i+1,taille-2))!= (char)32
-							&& StrB.charAt(Math.max(i-1,1))!= (char)32){
-						StrB.deleteCharAt(i);
-						taille = StrB.length();
-					}
+
+	public static void erase(String txt) {
+		StringBuilder str = new StringBuilder(txt.trim());
+		for(int i=0; i<str.length(); i++) {
+			if(str.charAt(i) == ' ') {
+				if(str.charAt(i+1) != ' ' && str.charAt(i-1) != ' ') {
+					str.deleteCharAt(i);
 				}
-				str = StrB.toString();
-				return str;
-		        }
+			}
+		}
+		System.out.println(str);
 	}
+}
