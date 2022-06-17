60fcfd30978999e7501782afd99bcc98c2515cab
Hepssylon <102019165+Hepssylon@users.noreply.github.com>
2022-06-17 18:15:37 +0200

    Update test java.java

--- test java.java
+++ test java.java
@@ -3,19 +3,48 @@ package eraser;
 public class Eraser {
 
 	public static void main(String[] args) {
-		erase("Cou cou  J M  B");
-	}
-
-
-	public static void erase(String txt) {
-		StringBuilder str = new StringBuilder(txt.trim());
-		for(int i=0; i<str.length(); i++) {
-			if(str.charAt(i) == ' ') {
-				if(str.charAt(i+1) != ' ' && str.charAt(i-1) != ' ') {
-					str.deleteCharAt(i);
-				}
-			}
-		}
+		String str = erase("Cou cou  J M  B");
 		System.out.println(str);
 	}
+
+
+	/***
+	 * Méthode permettant de supprimer les espaces simples et gardant les espaces multiples 
+	 * @param str
+	 * @return str 
+	 */
+    public static String erase(String str) {
+    	int tailleM ;
+    	String newStr = "";
+    	int cmpt = 0;
+        tailleM = str.length() ;
+        
+        for (int i = 0 ; i < tailleM ; i ++) {
+        	char caract = str.charAt(i);
+        	if (caract == ' ') {
+            	cmpt ++; // Compte du nombre d'espace 
+            }
+        	else { 
+        		if (cmpt > 1) { // Si le nombre  d'espace est supp�rieur � 1 alors on les affiches 
+        			for (int j = 0; j < cmpt ; j++) {
+        				newStr += ' ';
+        				}
+        		
+        		
+        		}
+        		cmpt = 0;
+        		newStr += caract ;
+        		}
+        		
+    
+    
+        }
+		
+        
+        
+        return newStr;
+    
+    
+    
+    }
 }
