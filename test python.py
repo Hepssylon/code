a614357e97320774a3e7a1fa90750de65b497281
Hepssylon <102019165+Hepssylon@users.noreply.github.com>
2022-06-17 17:12:22 +0200

    Update test python.py

--- test python.py
+++ test python.py
@@ -1,33 +1,16 @@
 str = "Cou cou  J M  B"
 
 # return string without spaces
-def erase(cc):
-
-    if len(cc) == 0:
-        return cc
-
-    #si le début de la chaîne n'est pas un espace
-    if cc[0] != " ":
-        res = cc[0]
-
-    #on part du deuxième caractère jusqu'à l'avant-dernier
-    #pour éviter les problèmes d'indices dans le test ci-dessous
-    i=1
-    while i<len(cc)-1:
-        #on concatène s'il n'y a pas d'espace
-        if cc[i] != " ":
-            res = res + cc[i]
-        else:
-            #on concatène si ce n'est pas un simple espace
-            if cc[i] == " " and (cc[i-1] == " " or cc[i+1] == " " ):
-                res = res + cc[i]
-        
-        i += 1
-
-    #si la fin de la chaîne n'est pas un espace
-    if cc[len(cc)-1] != " ":
-        res = res + cc[len(cc)-1]
-        
-    return res
+def erase(str):
+	result = ""
+	for i in range(len(str)):
+		if str[i] == " ":
+			if i != 0 and str[i-1] == " ":
+				result += str[i];
+			elif i+1 < len(str) and str[i+1] == " ":
+				result += str[i]
+		else:
+			result += str[i]
+	return result
 
 erase(str)
