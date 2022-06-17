f8333d42bd2d72e451fd4d73f99f6cd9f3a1f315
Hepssylon <102019165+Hepssylon@users.noreply.github.com>
2022-06-17 18:24:36 +0200

    Update test c.c

--- test c.c
+++ test c.c
@@ -1,37 +1,37 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-#include "solution.h"
 
-int main() {
-  
-    printf("%s\n",erase(""));
+/** Permet de supprimer seulement les espaces individuels d'une chaîne de caractère dans une nouvelle chaîne de caractère
+ * @param IN chaine : La chaîne de caractère à tester
+ **/  
+char* erase(char* chaine) {
+  //Déclaration des attributs nécessaires
+  char* newchaine = calloc(strlen(chaine),sizeof(int));
+  int i = 0;
+  int j = 0;
+   
+   while (chaine[i] != '\0') //Tant que la dernière case du tableau n'est pas atteinte
+   {
+       //Si des espaces consécutifs sont présents dans la chaines de caractères
+      if ((chaine[i] == ' ' && chaine[i+1] != ' ' && chaine[i-1] != ' ') != 1) {
+        newchaine[j] = chaine[i];
+        j++;
+      }
+      i++;
+   }
+   
+   //Dernière case du tableau
+   newchaine[j] = '\0';
+
+   //Affichage de la nouvelle chaîne (en respectant les conditions)
+   printf("Après la suppression des espaces :'%s' ", newchaine);
+    
+   //Libération de la mémoire 
+   free(newchaine);
 }
 
-char* erase(char* chaine) {
-    int i = 0, j = 0;
-    int taille = strlen(chaine);
-    char* newChaine = malloc( sizeof(char) * taille);
-    
-    while(chaine[i] != '\0'){
-        if(chaine[i] != ' '){
-          newChaine[j] = chaine[i];
-          j++;
-
-        }else if(chaine[i] == ' ' && chaine[i+1] == ' '){
-          newChaine[j] = chaine[i];
-          j++;
-
-        }else if( i > 0 && chaine[i] == ' ' && chaine[i-1] == ' '){
-          newChaine[j] = chaine[i];
-          j++;
-
-        }
-    i++;
-
-    }
-    
-    newChaine[j] = '\0';
-
-    return newChaine;
+int main(void) {
+  char* str = "Cou cou  J M  B";
+  erase(str);
 }
